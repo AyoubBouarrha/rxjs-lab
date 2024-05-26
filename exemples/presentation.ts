d66a1d8bab7f21827ef5c8d052aaff2db5c6fdb6
Ayoub Bouarrha <bouarrha.ayoub@gmail.com>
2024-05-27 01:00:38 +0200

    Update presentation.ts

--- exemples/presentation.ts
+++ exemples/presentation.ts
@@ -1,32 +1,29 @@
 import { textColor } from '../helper'
 
-// let test = (callback: Function) => {
-//     console.log(textColor('red','A'));
-//     callback();
-// }
-// test(() => console.log(textColor('yellow','B')));
+let test = (callback: Function) => {
+    console.log(textColor('red','A'));
+    callback();
+}
+test(() => console.log(textColor('yellow','B')));
 
 
+console.log("normal task");
+
+setTimeout(() => {
+    console.log("micro task");
+}, 5000);
 
 
+fetch('http://test.com/api/articles')//Promise
+  .then(response => console.log("macro task resonponse" , response));
+let btn : any
 
-// console.log("normal task");
+setTimeout(() => {console.log("after 3 sec")}, 3000);
 
-// setTimeout(() => {
-//     console.log("micro task");
-// }, 5000);
+setInterval(() => {console.log("each 2 sec")}, 2000);
 
-
-// fetch('http://test.com/api/articles')//Promise
-//   .then(response => console.log("macro task resonponse" , response));
-// let btn : any
-
-// setTimeout(() => {console.log("after 3 sec")}, 3000);
-
-// setInterval(() => {console.log("each 2 sec")}, 2000);
-
-// btn.addEventListener('click', () => {
-//     alert('You clicked me!');
-// });
+btn.addEventListener('click', () => {
+    alert('You clicked me!');
+});
 
 
